86143991b7dd107f56cd7e07a1ca261ea031d324
mellzin <ericavoltareli017@gmail.com>
2024-12-04 09:33:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é algoritmo?',
+    'o algoritmo é um sequencia de etapas bem definidas para resolver um problema específico'
+)
+
+criaCartao(
+    'matematica',
+    'o que é fracão?',
+    'A fração representa uma parte de um todo e é composta por dois numeros'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um index?',
+    'É um editor de texto'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'o que significa "sinônimos"?',
+    'são palavras que têm significados semelhantes ou iguais'
+)
